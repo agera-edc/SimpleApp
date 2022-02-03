1ca9b57615646195b9a786ea536caeca8b1ed79e
Izabela Kulakowska <ikulakowska@microsoft.com>
2022-02-03 16:04:25 +0100

    Change

--- app/src/main/java/simpleapp/App.java
+++ app/src/main/java/simpleapp/App.java
@@ -15,6 +15,7 @@ public class App {
     }
 
     public static void main(String[] args) {
+
         System.out.println(new App().getGreeting());
         System.out.println(new App().getAnotherGreeting());
     }
