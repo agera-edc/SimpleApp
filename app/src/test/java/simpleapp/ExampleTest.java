7684369845232a9478e6b15b45d2100db6340bf7
Izabela Kulakowska <ikulakowska@microsoft.com>
2022-02-03 16:44:11 +0100

    Add new class Example

---
+++ app/src/test/java/simpleapp/ExampleTest.java
@@ -0,0 +1,14 @@
+package simpleapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ExampleTest {
+
+
+    @Test
+    public void test() {
+        assertEquals(new Example().getData(), 42);
+    }
+}
